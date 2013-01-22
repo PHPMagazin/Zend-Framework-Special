7647e13c684c0ed2300cffb4390873b5441649a4
Michael <michael@abouttheweb.de>
2013-01-22 09:32:28 +0100

    Update ZF2-Cookbook/Listing_11.php

--- ZF2-Cookbook/Listing_11.php
+++ ZF2-Cookbook/Listing_11.php
@@ -1,1 +1,29 @@
-<?phpnamespace Zhorty\Controller;use Zend\Mvc\Controller\AbstractActionController;use Zend\View\Model\ViewModel;class TrimController extends AbstractActionController{    public function indexAction()    {        $sm = $this->getServiceLocator();        $form = $sm->get('Zhorty\Form\Trim');        return new ViewModel(array('form' => $form));    }}
+<?php
+namespace Zhorty\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class TrimController extends AbstractActionController
+{
+    private $trimForm;
+    
+    public function indexAction()
+    {
+        // Get the form in the service_manager
+        // configuration.
+        // $sm = $this->getServiceLocator();
+        // $form = $sm->get('Zhorty\Form\Trim');
+        return new ViewModel(array('form' => $this->getTrimForm()));
+    }
+    
+    public function setTrimForm($trimForm)
+    {
+        $this->trimForm = $trimForm;
+    }
+    
+    public function getTrimForm()
+    {
+        return $this->trimForm;
+    }
+}
